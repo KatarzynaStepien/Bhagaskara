a448e189e80010723baa73d58664d39849fd91cd
catalinka.28@gmail.com <catalinka.28@gmail.com>
2015-11-26 14:46:13 +0100

    contact

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 /**
  * Created by Kasia on 2015-11-20.
  */
+$(document).ready(function(){
+
+    var menu = $(".menu");
+    var menuStartPosition = menu.offset().top;
+    var spacer = $("#menuSpacer");
+
+    $(window).on("scroll", function(event){
+
+        if($(window).scrollTop() > menuStartPosition){
+            menu.addClass("sticky");
+            spacer.height(menu.height());
+        }else {
+            menu.removeClass("sticky");
+            spacer.height(0);
+        }
+    });
+});
