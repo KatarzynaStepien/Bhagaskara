7e238365a0220f8d74456807f21b376eedea2f69
catalinka.28@gmail.com <catalinka.28@gmail.com>
2015-11-27 12:34:40 +0100

    Hamburger menu

--- js/app.js
+++ js/app.js
@@ -19,6 +19,27 @@ $(document).ready(function(){
     });
 
 
+    function setMenu() {
+        $(window).on('resize', function() {
+            if ($(window).width() < 735) {
+                $('.nav').hide();
+                $('.menuBtn').show();
+            } else {
+                $('.nav').show();
+                $('.menuBtn').hide();
+                $('.mobileMenu').hide();
+            }
+        });
+    }
+
+    function setClick() {
+        $('.menuBtn').on('click', function() {
+            $('.mobileMenu').toggle();
+        });
+    }
+
+    //setMenu();
+    setClick();
 
 
 
